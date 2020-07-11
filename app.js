7e74d4e993c17bf885221a24171d5e867e877a11
Rakesh Sagar <rakeshsagar64@gmail.com>
2020-07-11 18:29:56 +0530

    aa

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ submitButton.addEventListener('click', () => {
       message: messageField.value
     };
     console.log('data--->', data);
-    postData('https://blooming-wave-70183.herokuapp.com//sendemail', data)
+    postData('https://blooming-wave-70183.herokuapp.com/sendemail', data)
   .then(data => {
     alert(data); // JSON data parsed by `data.json()` call
   });

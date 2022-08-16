3ba34c96f5c53f8cf61a7a05930fff78374b6c7f
Rakesh Sagar <Developer@mail.com>
2022-08-17 00:53:19 +0530

    contact us page and recruitment photo

--- script/uiTemplateInjector.js
+++ script/uiTemplateInjector.js
@@ -1,5 +1,7 @@
 const footer = document.querySelector('.footer');
 const contactCard = document.querySelector('#contactCard');
+const bangaloreContactCard = document.querySelector('#bangaloreContactCard');
+const ramnagarContactCard = document.querySelector('#ramnagarContactCard');
 const header = document.querySelector("#header");
 const ramnagarGoogleMap = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7783.64079634611!2d77.27578772310865!3d12.725136879414109!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae4de2c2e78587%3A0x638f610e2a869396!2sY%20Square%20Academy%20Pvt%20Ltd!5e0!3m2!1sen!2sin!4v1657218702400!5m2!1sen!2sin" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
 const nagarbhaviaddress = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.227363766768!2d77.51824741450136!3d12.957298018698582!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae3dde41c9d541%3A0xdd5400ac1d9e0a9a!2sYSquare%20Academy!5e0!3m2!1sen!2sin!4v1657218870898!5m2!1sen!2sin" width="100%" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
@@ -52,6 +54,49 @@ const loadContactCard = (e) => {
   }
 }
 
+const loadBangaloreContactCard = (e) => {
+  if(bangaloreContactCard){
+    bangaloreContactCard.innerHTML=` <div class="row">
+    <div class="col-xs-12">
+      <h4 class="subHeading">
+        <span class="btn-action-outline-rounded glyphicon glyphicon-envelope">
+        </span>&nbsp;
+        ysquare.33@gmail.com
+      </h4>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <h4 class="subHeading">
+        <span class="btn-action-outline-rounded glyphicon glyphicon-earphone">
+        </span>&nbsp;
+        9008811833
+      </h4>
+    </div>`;
+  }
+}
+
+const loadRamnagarContactCard = (e) => {
+  if(ramnagarContactCard){
+    ramnagarContactCard.innerHTML=` <div class="row">
+    <div class="col-xs-12">
+      <h4 class="subHeading">
+        <span class="btn-action-outline-rounded glyphicon glyphicon-envelope">
+        </span>&nbsp;
+        ysquare.33@gmail.com
+      </h4>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-xs-12">
+      <h4 class="subHeading">
+        <span class="btn-action-outline-rounded glyphicon glyphicon-earphone">
+        </span>&nbsp;
+        9742512616
+      </h4>
+    </div>`;
+  }
+}
 
 const loadFooter = (e) => {
   footer.innerHTML = `<div class='container-fluid copyright'>
@@ -135,10 +180,14 @@ window.addEventListener ?
     window.addEventListener("load", (e) => {
       loadHeader(e);
         loadContactCard(e);
+        loadBangaloreContactCard(e);
+        loadRamnagarContactCard(e)
         loadFooter(e);
     }, false) :
     window.attachEvent && window.attachEvent("onload", (e) => {
       loadHeader(e);
+      loadRamnagarContactCard(e);
+      loadBangaloreContactCard(e);
         loadContactCard(e);
         loadFooter(e);
     });
